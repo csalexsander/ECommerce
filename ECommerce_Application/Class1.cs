c47b2e0200f24d61b7ca934e108762212eb94e5e
Alexsander <alexsander@auvo.com.br>
2019-07-11 20:01:08 -0300

    Initial Commit

---
+++ ECommerce_Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ECommerce_Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ eCommerce_DI/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace eCommerce_DI
+{
+    public class Class1
+    {
+    }
+}
